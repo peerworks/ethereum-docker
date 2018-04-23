85d4ce7cce1ad4bb3f185a2c054a3c290ce8cfb8
sjkyspa <sjkyspa@gmail.com>
2018-04-23 12:17:05 +0800

    fix genesis mounmt

--- netstats/rootfs/bin/geth.sh
+++ netstats/rootfs/bin/geth.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/bin/geth --networkid=331788 --bootnodes="enode://f5e80a12c9aa63ba2ce96d03c13eaf2bafd295f62383feb0af383d29a0c7d78c80049fd63671e703564b400b890e5e1afabece47939883cc8723ffd14976ab6a@172.16.238.10:30301" --rpc --rpcaddr="0.0.0.0" --rpcapi="db,eth,net,web3,personal" --rpccorsdomain="*" --cache=200 --verbosity=4 --port=30305 node
+/usr/local/bin/geth --datadir=/ethereum/data --ethash.dagdir=/ethereum/ethash --networkid=331788 --bootnodes="enode://f5e80a12c9aa63ba2ce96d03c13eaf2bafd295f62383feb0af383d29a0c7d78c80049fd63671e703564b400b890e5e1afabece47939883cc8723ffd14976ab6a@172.16.238.10:30301" --rpc --rpcaddr="0.0.0.0" --rpcapi="db,eth,net,web3,personal" --rpccorsdomain="*" --cache=200 --verbosity=4 --port=30305 node
